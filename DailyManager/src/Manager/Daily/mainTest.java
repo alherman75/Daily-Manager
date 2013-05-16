0da9e397dd47d021f95ef5e5951a35dd3c8d9f8a
alherman75 <reeses624@gmail.com>
2013-05-16 11:31:25 -0400

    start

---
+++ DailyManager/src/Manager/Daily/mainTest.java
@@ -0,0 +1,13 @@
+package Manager.Daily;
+
+public class mainTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This love.");
+	}
+
+}
